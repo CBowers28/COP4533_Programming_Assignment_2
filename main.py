ee21676ad99a192db47c548c6f309b50294a8f8f
CBowers28 <148987810+CBowers28@users.noreply.github.com>
2026-02-24 14:15:08 -0500

    Did input parsing and FIFO

---
+++ main.py
@@ -0,0 +1,24 @@
+#input parsing
+k, m = map(int, input().split())
+seq = list(map(int, input().split()))
+
+
+
+def FIFO(k, m, seq):
+    cache = []
+    hits = 0
+    misses = 0
+    for num in seq:
+        if num in cache:
+            hits += 1
+        else:
+            misses += 1
+            if len(cache) >= k:
+                cache.pop(0)
+                cache.append(num)
+            else:
+                cache.append(num)
+
+    return misses, hits
+
+print(FIFO(k, m, seq))
