276623f9f21ce4d6138890b0b8fdf51d5f5e51cd
CBowers28 <148987810+CBowers28@users.noreply.github.com>
2026-02-25 11:58:01 -0500

    Added LRU

--- main.py
+++ main.py
@@ -21,4 +21,25 @@ def FIFO(k, m, seq):
 
     return misses, hits
 
+def LRU(k, m, seq):
+    cache = []
+    hits = 0
+    misses = 0
+    for num in seq:
+        if num in cache:
+            hits += 1
+            cache.remove(num)
+            cache.append(num)
+        else:
+            misses += 1
+            if len(cache) >= k:
+                cache.pop(0)
+                cache.append(num)
+            else:
+                cache.append(num)
+    return misses, hits
+
+
 print(FIFO(k, m, seq))
+print(LRU(k, m, seq))
+
