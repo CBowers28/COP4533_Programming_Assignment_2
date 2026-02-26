3740768b666778db112b349a3920bdffc4874919
CBowers28 <148987810+CBowers28@users.noreply.github.com>
2026-02-26 11:41:47 -0500

    Added OPTFF

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #input parsing
+'''
+input example:
+3 15
+1 2 3 4 1 2 3 4 1 2 3 4 5 1 2
+'''
 k, m = map(int, input().split())
 seq = list(map(int, input().split()))
 
@@ -39,7 +44,37 @@ def LRU(k, m, seq):
                 cache.append(num)
     return misses, hits
 
+def longest_next(cache, future):
+    furthest_item = None
+    furthest_dist = -1
+    for item in cache:
+        if item not in future:
+            return item
+        dist = future.index(item)  # first occurrence
+        if dist > furthest_dist:
+            furthest_dist = dist
+            furthest_item = item
+    return furthest_item
+
+def OPTFF(k, m, seq):
+    cache = []
+    hits = 0
+    misses = 0
+    for i in range(len(seq)):
+        if seq[i] in cache:
+            hits += 1
+        else:
+            misses += 1
+            if len(cache) >= k:
+                cache.remove(longest_next(cache, seq[i:]))
+                cache.append(seq[i])
+            else:
+                cache.append(seq[i])
+    return misses, hits
+
+
+
 
 print(FIFO(k, m, seq))
 print(LRU(k, m, seq))
-
+print(OPTFF(k, m, seq))
